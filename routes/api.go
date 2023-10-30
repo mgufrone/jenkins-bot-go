9dd63995295b19a4220de7df57704addac6fdf09
Mochamad Gufron <mgufronefendi@gmail.com>
2023-10-30 18:59:25 +0700

    fix: disable unused routes

--- routes/api.go
+++ routes/api.go
@@ -1,15 +1,4 @@
 package routes
 
-import (
-	"github.com/goravel/framework/contracts/route"
-	"github.com/goravel/framework/facades"
-
-	"mgufrone.dev/job-tracking/app/http/controllers"
-)
-
 func Api() {
-	userController := controllers.NewUserController()
-	facades.Route().Prefix("/users").Group(func(router route.Router) {
-		router.Get("/{id}", userController.Show)
-	})
 }
